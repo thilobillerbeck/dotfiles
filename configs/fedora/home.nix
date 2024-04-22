b3b0098eb5c012dbe82e7de5dff253f419b2db7e
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-04-22 17:50:39 +0200

    several nixgl things

--- configs/fedora/home.nix
+++ configs/fedora/home.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ pkgs, config, lib, inputs, ... }:
 
-{
+let
+  nixGL = import ./../../home-manager/utils/nixGLWrap.nix { inherit pkgs config; };
+in {
   imports = [ ./../../home-manager/modules/machine.nix ];
 
   machine = {
@@ -14,4 +16,11 @@
   fonts.fontconfig.enable = true;
 
   nixpkgs.config.allowUnfree = true;
+
+  nixGLPrefix = lib.getExe pkgs.nixgl.nixGLIntel;
+
+  home.packages = with pkgs; [
+    (nixGL insomnia)
+    (nixGL inputs.muse-sounds-manager.packages.x86_64-linux.muse-sounds-manager)
+  ];
 }
--- home-manager/utils/nixGLWrap.nix
+++ home-manager/utils/nixGLWrap.nix
@@ -1,17 +1,37 @@
-{ pkgs, lib, ... }:
+# Call once on import to load global context
+{
+  pkgs,
+  config,
+}:
+# Wrap a single package
+pkg:
+if config.nixGLPrefix == ""
+then pkg
+else
+  # Wrap the package's binaries with nixGL, while preserving the rest of
+  # the outputs and derivation attributes.
+  (pkg.overrideAttrs (old: {
+    name = "nixGL-${pkg.name}";
+    buildCommand = ''
+      set -eo pipefail
 
-let nixgl = import <nixgl> { };
-in pkg:
-pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
-  mkdir $out
-  ln -s ${pkg}/* $out
-  rm $out/bin
-  mkdir $out/bin
-  for bin in ${pkg}/bin/*; do
-  wrapped_bin=$out/bin/$(basename $bin)
-  echo -e "#!/bin/bash\nexec ${
-    lib.getExe nixgl.auto.nixGLDefault
-  } $bin \$@" > $wrapped_bin
-  chmod +x $wrapped_bin
-  done
-''
+      ${
+        # Heavily inspired by https://stackoverflow.com/a/68523368/6259505
+        pkgs.lib.concatStringsSep "\n" (map (outputName: ''
+          echo "Copying output ${outputName}"
+          set -x
+          cp -rs --no-preserve=mode "${pkg.${outputName}}" "''$${outputName}"
+          set +x
+        '') (old.outputs or ["out"]))
+      }
+
+      rm -rf $out/bin/*
+      shopt -s nullglob # Prevent loop from running if no files
+      for file in ${pkg.out}/bin/*; do
+        echo "#!${pkgs.bash}/bin/bash" > "$out/bin/$(basename $file)"
+        echo "exec -a \"\$0\" ${config.nixGLPrefix} $file \"\$@\"" >> "$out/bin/$(basename $file)"
+        chmod +x "$out/bin/$(basename $file)"
+      done
+      shopt -u nullglob # Revert nullglob back to its normal default state
+    '';
+  }))
