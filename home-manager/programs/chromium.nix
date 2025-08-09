1dfec5dc150c14c4861c55ba379a459e64649404
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-08-10 00:12:17 +0200

    remove unused chromium vars

--- home-manager/programs/chromium.nix
+++ home-manager/programs/chromium.nix
@@ -1,38 +1,7 @@
 { pkgs, config, ... }:
 
 let
-  chromium_extension = [
-    "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
-    "lhobafahddgcelffkeicbaginigeejlf" # Allow Cors
-    "dnhpnfgdlenaccegplpojghhmaamnnfp" # Augmented Steam
-    "mdjildafknihdffpkfmmpnpoiajfjnjd" # Consent-O-Matic
-    "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-    "mnjggcdmjocbbbhaepdhchncahnbgone" # Sponsor Block
-    "epocinhmkcnjfjobnglchpbncndobblj" # Mastodon Gaze
-    "blipmdconlkpinefehnmjammfjpmpbjk" # Lighthouse
-    "ggijpepdpiehgbiknmfpfbhcalffjlbj" # Open in mpv
-    "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock Origin
-    "gbmdgpbipfallnflgajpaliibnhdgobh" # JSON Viewer
-    "nhdogjmejiglipccpnnnanhbledajbpd" # Vue Devtools
-    "blipmdconlkpinefehnmjammfjpmpbjk" # Lighthouse
-    "fmkadmapgofadopljbjfkapdkoienihi" # React Devtools
-    "bmnlcjabgnpnenekpadlanbbkooimhnj" # Honey
-    "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly
-  ];
   isEnabled = if (config.machine.isGraphical) then true else false;
-  dictionaries = with pkgs.hunspellDictsChromium; [
-    en_US
-    de_DE
-  ];
-  commandLineArgs = [
-    "--enable-wayland-ime"
-    "--ignore-gpu-blocklist"
-    "--enable-gpu-rasterization"
-    "--enable-zero-copy"
-    "--disable-features=UseChromeOSDirectVideoDecoder"
-    "--enable-features=WaylandWindowDecorations,VaapiVideoDecoder,VaapiVideoEncoder,VaapiVideoDecodeLinuxGL,WebRTCPipeWireCapturer"
-  ];
-  extensions = map (eid: { id = eid; }) chromium_extension;
   nixGL = config.lib.nixGL.wrap;
 in
 {
