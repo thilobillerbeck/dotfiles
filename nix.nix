bffc5d1d9c23d5e597704198f92836e7832c4d53
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-01-11 23:34:27 +0100

    adjust nix settings

--- nix.nix
+++ nix.nix
@@ -18,13 +18,16 @@
         "https://nixpkgs-update.cachix.org"
         "https://devenv.cachix.org"
         "https://cache.garnix.io"
+        "https://cache.lix.systems"
       ];
+      auto-optimise-store = true;
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       ];
     };
   };
