5d477a0f19593426d8e8548bac28a501c7bb0ecf
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-08-01 21:51:34 +0200

    remove garnix cache

--- nix.nix
+++ nix.nix
@@ -16,7 +16,6 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://devenv.cachix.org"
-        "https://cache.garnix.io"
         "https://cache.lix.systems"
       ];
       auto-optimise-store = true;
@@ -24,7 +23,6 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       ];
     };
